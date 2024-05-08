4e5c55a3efb994d0364ca2f021f56bb3b3a3e7fa
mona <mona-rea.nerva@voco.ee>
2024-05-08 14:10:13 +0300

    Created TodoController.getTodos unit test and integration tests

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -24,4 +24,11 @@ describe(endpointUrl, () => {
             })
         }
     )
+    test("GET " + endpointUrl, async () => {
+        const response = await request(app).get(endpointUrl)
+        expect(response.statusCode).toBe(200)
+        expect(Array.isArray(response.body)).toBeTruthy()
+        expect(response.body[0].title).toBeDefined()
+        expect(response.body[0].done).toBeDefined()
+    })
 })
--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -2,8 +2,10 @@ const TodoController = require('../../controllers/todo.controller')
 const TodoModel = require('../../models/todo.model')
 const httpMocks = require('node-mocks-http')
 const newTodo = require('../mock-data/new-todo.json')
+const allTodos = require("../mock-data/all-todos.json")
 
 TodoModel.create = jest.fn()
+TodoModel.find = jest.fn()
 
 let req, res, next
 beforeEach(() => {
@@ -12,7 +14,7 @@ beforeEach(() => {
     next = jest.fn()
 })
 
-describe('TodoController.createTodo', () => {
+describe('TodoController.getTodo', () => {
     beforeEach(() => {
         req.body = newTodo
     })
@@ -40,6 +42,31 @@ describe('TodoController.createTodo', () => {
         const rejectedPromise = Promise.reject(errorMessage)
         TodoModel.create.mockReturnValue(rejectedPromise)
         await TodoController.createTodo(req, res, next)
+        expect(next).toBeCalledWith(errorMessage)
+    })
+})
+
+describe("TodoController.getTodos", () => {
+    it("should have a getTodos function", () => {
+        expect(typeof TodoController.getTodos).toBe('function')
+    })
+
+    it("should call TodoModel.find({})", async () => {
+        await TodoController.getTodos(req, res, next);
+        expect(TodoModel.find).toHaveBeenCalledWith({})
+    })
+    it("should return response with status 200 and all todos", async () => {
+        TodoModel.find.mockReturnValue(allTodos)
+        await TodoController.getTodos(req, res, next)
+        expect(res.statusCode).toBe(200)
+        expect(res._isEndCalled()).toBeTruthy();
+        expect(res._getJSONData()).toStrictEqual(allTodos);
+    })
+    it("should handle errors in getTodos", async () => {
+        const errorMessage = { message: "Done property missing" }
+        const rejectedPromise = Promise.reject(errorMessage)
+        TodoModel.create.mockReturnValue(rejectedPromise)
+        await TodoController.getTodo(req, res, next)
         expect(next).toBeCalledWith(errorMessage)
     })
 })
